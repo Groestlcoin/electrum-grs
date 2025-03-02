d3f4820833b3cfefa6a22b85f703cc9d73cd488f
gruve-p <jackielove4u@hotmail.com>
2025-03-02 22:17:00 +0100

    Update __init__.py

--- electrum_grs/plugins/watchtower/__init__.py
+++ electrum_grs/plugins/watchtower/__init__.py
@@ -2,15 +2,21 @@ from electrum_grs.i18n import _
 
 fullname = _('Watchtower')
 description = """
-Watchtower for Electrum-GRS.
+A watchtower is a daemon that watches your channels and prevents the other party from stealing funds by broadcasting an old state.
 
-Example setup:
+Example:
+
+daemon setup:
 
   electrum-grs -o setconfig enable_plugin_watchtower True
   electrum-grs -o setconfig watchtower_user wtuser
   electrum-grs -o setconfig watchtower_password wtpassword
   electrum-grs -o setconfig watchtower_port 12345
   electrum-grs daemon -v
+
+client setup:
+
+  electrum-grs -o setconfig watchtower_url http://wtuser:wtpassword@127.0.0.1:12345
 
 """
 
